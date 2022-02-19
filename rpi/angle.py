202a7ab3c45168110b20316db928f4590290fd1a
psampson <unconfigured@null.spigotmc.org>
2022-02-18 18:17:14 -0700

    Made Rpi code functional

--- rpi/angle.py
+++ rpi/angle.py
@@ -11,6 +11,8 @@ Arduino for angles from -360 to 360 degrees.
 
 from smbus2 import SMBus
 
+bus = SMBus(1)
+
 angle_codes = {
     -90: 0x04,
     -180:0x03,
@@ -23,17 +25,16 @@ angle_codes = {
     360: 0x01,
     }
 
-#bus = SMBus(1)
 
 def SendRecieve (angle):
-    if(-360 <= angle < 0):
+    if(-360 <= angle < 0): #Add 360 degrees to negative angles
         angle += 360
     if(0 <= angle <= 360):
+        
             angle_code = angle/90
             angle_code = round(angle_code)+1
             print ("Sending command %i to turn the wheel to %i " % (angle_code, angle))
-            #bus.write_byte_data(0x04, 0,angle_codes(angle))
+            bus.write_byte_data(0x04, 0,angle_codes(angle))
             return 5
     else: print ("Please enter an angle from -360 to 360 degrees")
 
-SendRecieve(95)
