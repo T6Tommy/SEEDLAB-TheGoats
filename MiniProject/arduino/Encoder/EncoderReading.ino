da6a7bee388b49f69851619a3f7f0e7644c79d6a
T6Tommy <99838940+T6Tommy@users.noreply.github.com>
2022-02-27 19:52:24 -0700

    Add files via upload

---
+++ MiniProject/arduino/Encoder/EncoderReading.ino
@@ -0,0 +1,43 @@
+// SEED Lab - Mini Project section 4.4
+// Thomas MacDougall - Group 7
+// Srping 2022
+
+// 3200 counts is a fill rotation
+#include <Encoder.h>
+
+Encoder myEnc(3, 4); // Pin a from the motor is attached to digital pin 3, and pin b is attached to digital pin 4
+int PWMpin=9;
+
+const int buttonPin = 8; // Pin for the button is digital pin 8
+int buttonState = 0;   // declaration for button state
+
+void setup() {
+  Serial.begin(250000); 
+  pinMode(buttonPin, INPUT);
+}
+
+long oldPosition  = -999;
+const float delta = 0.001963495; // This is [(2*pi) / 3200]
+float theta = 0; // initilaze theta
+
+void loop() {
+  if (millis() > 1000 && millis() < 2000) analogWrite(PWMpin, 200);
+  else analogWrite(PWMpin, 0);
+  long newPosition = myEnc.read();
+
+  if (newPosition != oldPosition) { // if the wheel has moved
+    if(newPosition > oldPosition) { // if going forward
+      theta += delta;               // increads theta by delta
+    }
+    else {                          // if going backwards
+      theta = (theta - delta);      // decrease theta by delta
+    }
+    oldPosition = newPosition;      // sets new position for next loop
+    Serial.println(theta);
+  }
+  
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) { // if button pressed
+    theta = 0; // reset theta
+  }
+}
