8825a6d9f82402926722b331f23c4c7865b720de
psampson <unconfigured@null.spigotmc.org>
2022-03-02 13:30:32 -0700

    added DistanceTest.ino

---
+++ Demo1/arduino/DistanceTest/DistanceTest.ino
@@ -0,0 +1,19 @@
+#define M1PWM 9
+#define M2PWM 10
+#define M1DIR 7
+#define M2DIR 8
+
+#define M1FB A0
+#define M2FB A1
+
+#define EN 4
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
