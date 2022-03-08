05527001dd6759ae38a0de024a9f842a95b05cb3
psampson <unconfigured@null.spigotmc.org>
2022-03-08 07:52:43 -0700

    added peripheral-side (arduino) i2c floating-point transfer method.

--- Demo1/arduino/DistanceTest/DistanceTest.ino
+++ Demo1/arduino/DistanceTest/DistanceTest.ino
@@ -6,11 +6,18 @@
 #define M1FB A0
 #define M2FB A1
 
+#define ENC1 3
+#define ENC2 5
+
 #define EN 4
 
 void setup() {
   // put your setup code here, to run once:
-
+  digitalWrite(EN, LOW);
+  pinMode(M1FB, INPUT);
+  pinMode(M2FB, INPUT);
+  pinMode(ENC1, INPUT);
+  pinMode(ENC2, INPUT);
 }
 
 void loop() {
